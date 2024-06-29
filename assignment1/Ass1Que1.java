b7fe2dd7f7b8cd5836eb7022131cc021f1291a31
Piyush <theofficeofpiyush@gmail.com>
2024-06-30 02:07:59 +0530

    Added Ass1

--- assignment1/Ass1Que1.java
+++ assignment1/Ass1Que1.java
@@ -1,5 +1,7 @@
 public class Ass1Que1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello Piyush");
+        System.out.println("Hello MySirG");
     }
 }
