7adceb16e19b74e41c7564c2f7fd729f0d94d1b6
Piyush <theofficeofpiyush@gmail.com>
2024-07-12 19:07:10 +0530

    fo

--- assignment1/Ass1Que2.java
+++ assignment1/Ass1Que2.java
@@ -2,6 +2,6 @@ public class Ass1Que2 {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Student");
-        System.out.println("Hiiiiiiii");
+        System.out.println("Hiiiiiiiii");
     }
 }
---
+++ assignment8/Find_output.java
@@ -0,0 +1,12 @@
+public class Find_output {
+    private int y;
+    private static int z;
+    public static void main (String[] args){
+        int x;
+        System.out.println("x="+x);
+        System.out.println("y="+y);
+        System.out.println("z="+z);
+
+    }
+}
+
